c08965793e365b706335800129693e35d50652c0
arina topor <arinatopor695@gmail.com>
2023-10-30 16:23:22 +0500

    Init Planning Module

--- Host/src/App.tsx
+++ Host/src/App.tsx
@@ -1,14 +1,14 @@
-import './App.css'
-import PerformanceReview from 'performanceReview/PerformanceReview'
-
+import "./App.css";
+import PerformanceReview from "performanceReview/PerformanceReview";
+import Planning from "planning/Planning";
 
 function App() {
-
   return (
     <div>
-      <PerformanceReview isMicroApp={true}/>
+      <PerformanceReview isMicroApp={true} />
+      <Planning isMicroApp={true} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Planning/src/pages/Planning.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "antd";
+
+export default function Planning() {
+  return (
+    <Layout>
+      <Layout.Content>
+        <p>Модуль 360</p>
+      </Layout.Content>
+    </Layout>
+  );
+}
