5aabc185f9a508bc586f57aecdf070ee98e475e1
dzharvizzz-coder <Dzharvizzz@yandex.ru>
2023-11-26 19:41:38 +0500

    replace document.cookies to universal-cookie

--- Authorization/src/pages/Authorization.tsx
+++ Authorization/src/pages/Authorization.tsx
@@ -1,11 +1,9 @@
 import { useEffect } from 'react';
-import {Button, Flex, Form, Layout } from 'antd';
+import { Flex, Form, Layout } from 'antd';
 import './Authorization.scss';
 import { AuthButton } from "../components/AuthButton.ts";
-import { useGetProfileMutation } from '../store/Auth.ts';
 
 export default function Authorization() {
-    const [mutation] = useGetProfileMutation({})
     useEffect(() => {
         if (!window.YaAuthSuggest) {
             AuthButton();
@@ -21,17 +19,6 @@ export default function Authorization() {
                         <div id="buttonContainerId" className={'authorization-button'}></div>
                     </Form>
                     <p className={'authorization-text'}>Авторизация поможет вам сохранить ваши данные в безопасности</p>
-                    <Button onClick={async () => {
-                        const response = await mutation({})
-                        if (!('error' in response)) {
-                            console.log(response)
-                        } else {
-                            console.log('error')
-                            console.log(response)
-                        }
-                    }}>
-                        data
-                    </Button>
                 </Flex>
             </Layout.Content>
         </Layout>
