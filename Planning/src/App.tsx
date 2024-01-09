49aa8d14aa55e698a73727e2d7e204780dd1c3e2
arina topor <arinatopor695@gmail.com>
2024-01-09 22:27:37 +0500

    add menu

--- Planning/src/App.tsx
+++ Planning/src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ConfigProvider, ThemeConfig } from 'antd';
 import Planning from './pages/Planning';
 import locale from 'antd/locale/ru_RU';
+import Menu from 'host/Menu';
 
 const basePath = '/planning';
 function App({ isMicroApp }: { isMicroApp?: boolean }) {
     return (
         <>
             <ConfigProvider theme={theme} locale={locale}>
+                <Menu />
                 <BrowserRouter>
                     <Routes>
                         <Route path={basePath} element={<Planning />} />
@@ -29,6 +31,19 @@ export default App;
 
 const theme: ThemeConfig = {
     components: {
+        Slider: {
+            railSize: 15,
+            trackBg: '#3A6EFF',
+            trackHoverBg: '#3A6EFF',
+            railBg: '#98B3FE',
+            railHoverBg: '#98B3FE',
+            handleLineWidth: 1,
+            handleLineWidthHover: 2,
+            handleSize: 36,
+            handleSizeHover: 36,
+            borderRadiusXS: 22,
+        },
+
         Button: {
             fontSize: 16,
             colorPrimary: '#0843DD',
