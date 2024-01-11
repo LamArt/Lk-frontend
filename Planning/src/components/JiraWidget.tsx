18d413ebb82bf9a341a1a8367071d4629f6a7e35
arina topor <arinatopor695@gmail.com>
2024-01-11 13:19:25 +0500

    add jira-popup

--- Planning/src/components/JiraWidget.tsx
+++ Planning/src/components/JiraWidget.tsx
@@ -1,45 +1,107 @@
 import './JiraWidget.scss';
 import JiraCard from './UI/JiraCard';
-import { useGetIssuesQuery, Issue } from '../store/jiraApi/issueApi';
-import { useEffect, useState } from 'react';
+import {
+    useGetIssuesQuery,
+    Issue,
+    useGetJiraTokenMutation,
+    Error,
+} from '../store/jiraApi/issueApi';
+import { useCallback, useEffect, useState } from 'react';
 import { Spin } from 'antd';
-
+import { useLocation } from 'react-router-dom';
+import Cookies from 'universal-cookie';
+import JiraPopup from './UI/JiraPopup';
 const JiraWidget = () => {
-    const { data, refetch, isLoading } = useGetIssuesQuery();
+    const { data, refetch, isLoading, error } = useGetIssuesQuery();
     const [issuesList, setIssuesList] = useState<Issue[]>([]);
-    useEffect(() => {
-        const fetchIssues = async () => {
-            try {
-                await refetch();
-                if (data !== undefined) {
-                    setIssuesList(data);
-                    console.log(Object.entries(data));
-                }
-            } catch (error) {
-                console.log('Error post data:', error);
-            }
-        };
-        fetchIssues();
-        const intervalId = setInterval(() => {
-            fetchIssues();
-        }, 300000); //5 минут
+    const OAUTH_URL = String(import.meta.env.VITE_OAuth_Jira);
+    const [showPopup, setShowPopup] = useState(false);
+    const [mutation] = useGetJiraTokenMutation();
+    const location = useLocation();
+    const getCodeFromUrl = useCallback(() => {
+        const url = new URL(
+            location.pathname + location.search,
+            window.location.origin
+        );
+        const code = url.searchParams.get('code');
+        return code;
+    }, [location]);
 
-        return () => clearInterval(intervalId);
-    }, [data, refetch]);
+    const getJiraToken = useCallback(
+        async (code: string) => {
+            const cookies = new Cookies();
+
+            const responce = await mutation({ authorization_code: code });
+            if (responce !== undefined && 'data' in responce) {
+                setShowPopup(false);
+                cookies.set('jira_access_token', responce.data.access);
+            }
+        },
+        [location]
+    );
+
+    useEffect(() => {
+        const code = getCodeFromUrl();
+        if (code) {
+            getJiraToken(code);
+        }
+    }, [getCodeFromUrl]);
+
+    useEffect(() => {
+        if (
+            (error as Error)?.status === 400 ||
+            (error as Error)?.status === 401
+        ) {
+            setShowPopup(true);
+        }
+        if (data !== undefined) {
+            setIssuesList(data);
+        }
+    }, [isLoading, error, refetch]);
+
+    // useEffect(() => {
+    //     const fetchIssues = async () => {
+    //         try {
+    //             await refetch();
+    //             if (data !== undefined) {
+    //                 setIssuesList(data);
+    //                 console.log(Object.entries(data));
+    //             }
+    //         } catch (error) {
+    //             console.log('Error post data:', error);
+    //         }
+    //     };
+    //     fetchIssues();
+    //     const intervalId = setInterval(() => {
+    //         fetchIssues();
+    //     }, 300000); //5 минут
+
+    //     return () => clearInterval(intervalId);
+    // }, [data, refetch]);
     return (
         <>
-            <div className='jiraLink'>
+            <div className="jiraLink">
                 <a
-                href = 'https://lamart.atlassian.net/jira/your-work' 
-                target="_blank">
-                    <img src = {"/icons/jiraIcon.svg"}></img> 
-                    <p className='jiraLabel'>Jira</p>
+                    href="https://lamart.atlassian.net/jira/your-work"
+                    target="_blank"
+                >
+                    <img src={'/icons/jiraIcon.svg'}></img>
+                    <p className="jiraLabel">Jira</p>
                 </a>
             </div>
-            
+
             <div className="jira-window-container">
                 <div className="jira-tasks-container">
-                    {isLoading && <Spin size="large" style={{ margin: 'auto' }} />}
+                    {data === undefined && showPopup && !isLoading && (
+                        <JiraPopup
+                            onAuthorize={() =>
+                                (window.location.href = OAUTH_URL)
+                            }
+                        />
+                    )}
+                    {isLoading && (
+                        <Spin size="large" style={{ margin: 'auto' }} />
+                    )}
 
                     {!isLoading && !issuesList && (
                         <p
@@ -65,7 +127,6 @@ const JiraWidget = () => {
                 </div>
             </div>
         </>
-        
     );
 };
 
--- Planning/vite.config.ts
+++ Planning/vite.config.ts
@@ -1,33 +1,34 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import federation from "@originjs/vite-plugin-federation";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "planning",
-      filename: "remoteEntry.js",
-      remotes: {
-        authorization: "http://localhost:5002/assets/remoteEntry.js",
-      },
-      exposes: { "./Planning": "./src/App.tsx" },
-      shared: [
-        "react",
-        "react-dom",
-        "antd",
-        "react-router-dom",
-        "@reduxjs/toolkit",
-        "react-redux",
-        "react-query",
-      ],
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
+    plugins: [
+        react(),
+        federation({
+            name: 'planning',
+            filename: 'remoteEntry.js',
+            remotes: {
+                authorization: 'http://localhost:5002/assets/remoteEntry.js',
+                salary: 'http://localhost:5004/assets/remoteEntry.js',
+            },
+            exposes: { './Planning': './src/App.tsx' },
+            shared: [
+                'react',
+                'react-dom',
+                'antd',
+                'react-router-dom',
+                '@reduxjs/toolkit',
+                'react-redux',
+                'react-query',
+            ],
+        }),
+    ],
+    build: {
+        modulePreload: false,
+        target: 'esnext',
+        minify: false,
+        cssCodeSplit: false,
+    },
 });
