ceee3d3431dec5e3edc5671518bc83d1e485562b
Леонид Ершов <lenya.ershov.2001@mail.ru>
2024-01-13 23:18:08 +0500

    Add request for list lead and head from server

--- PerformanceReview/src/pages/PeersListForHead.tsx
+++ PerformanceReview/src/pages/PeersListForHead.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import {Select, Card, Flex, Layout, Form, Button} from "antd";
 import '../styles/Review.scss';
 import '../styles/PeerList.scss';
-import {peersListMock} from "../mocks/mocks";
-import {Peer} from "../types/peer";
+import {Teammate, useGetTeammatesQuery} from "../store/reviewApi/reviewApi";
+import manPhoto from "../assets/man.png";
 
 function PeersListForHead() {
+  const {data: teammatesData} = useGetTeammatesQuery()
+  const teammates = teammatesData?.teammates
+
   return (
     <Layout className="review">
       <Layout.Content className="review-content">
@@ -15,12 +18,12 @@ function PeersListForHead() {
             className='peerlist'
           >
             <Flex vertical gap={15} align={"center"}>
-              {peersListMock.map((peer: Peer) => (
+              {teammates && teammates.map((peer: Teammate) => (
                 <Card className="peerlist-card">
                   <Flex vertical={false} justify={'space-between'} className="peerlist-card-content">
                     <Flex vertical={false} gap={30} align={"center"}>
-                      <img alt={peer.name} className='peerlist-image' src={peer.image}/>
-                      <p className="peerlist-text">{peer.name}</p>
+                      <img alt={peer.last_name} className='peerlist-image' src={manPhoto}/>
+                      <p className="peerlist-text">{`${peer.first_name} ${peer.last_name}`}</p>
                     </Flex>
                     <Form.Item className='peerlist-select'>
                       <Select
